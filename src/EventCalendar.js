08272e9788ccc10d1a33f5df364d053ac64f2f07
Hugo <hugo@seedstars.com>
2019-03-28 17:18:00 +0000

    Fix null

--- src/EventCalendar.js
+++ src/EventCalendar.js
@@ -105,7 +105,7 @@ export default class EventCalendar extends React.Component {
                       {leftIcon}
                     </TouchableOpacity>
                     :
-                    null
+                    <View/>
                   }
                     <TouchableOpacity
                         onPress={this.props.calendarTapped}
@@ -123,7 +123,7 @@ export default class EventCalendar extends React.Component {
                       {rightIcon}
                     </TouchableOpacity>
                     :
-                    null
+                    <View/>
                   }
                 </View>
                 <DayView
